36b37f3bb4be51e6d9b05e247c253df9def8237a
Azzaare <jf@baffier.fr>
2023-04-03 20:15:35 +0900

    Basic interface for AbstractBot

--- src/ChatThemAll.jl
+++ src/ChatThemAll.jl
@@ -1,5 +1,19 @@
 module ChatThemAll
 
-# Write your package code here.
+export token, url, api_version, api_endpoint, channels, channel
+
+abstract type AbstractBot end
+
+token(bot::AbstractBot) = bot.token
+
+url(bot::AbstractBot) = bot.url
+
+api_version(bot::AbstractBot) = bot.api_version
+
+api_endpoint(bot::AbstractBot) = "$(url(bot))/api/$(api_version(bot))"
+
+channels(bot::AbstractBot) = bot.channels
+
+channel(bot::AbstractBot, channel_name::String) = channels(bot)[channel_name]
 
 end
